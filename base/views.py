8c78e54e3a7390a323a55824c51784b16dad493e
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-14 13:52:34 -0600

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
+from .models import Task
 
 
-def task_list(request):
-    return HttpResponse("Hi I'm a todo list")
+class TaskList(ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = "tasks"
+    paginate_by = 5
+    ordering = ["-complete"]
+
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = "task.html"
+    context_object_name = "task"
