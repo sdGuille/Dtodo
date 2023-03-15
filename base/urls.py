b63aa6487422e96019a04958f5763eb8e688227c
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-15 11:18:00 -0600

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,9 +5,13 @@ from .views import (
     TaskCreate,
     TaskUpdate,
     TaskDelete,
+    CustomLoginView,
 )
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login', CustomLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page="login"), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
