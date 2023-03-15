d91d55cad1f8106b92375c9e0aa11ec4cdb8bfae
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-15 09:41:42 -0600

    fixing typo errors

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView,
 )
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 # Create your views here.
 from .models import Task
