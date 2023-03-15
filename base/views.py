e9cc7213d0a56dc9769c05165959b8bb076e2fbb
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-14 21:39:23 -0600

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 from .models import Task
 
@@ -16,3 +18,10 @@ class TaskDetail(DetailView):
     model = Task
     template_name = "task.html"
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
